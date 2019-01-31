34c28a991b90b40b111a25aed8faa18a059ec39e
agam <agamgupta1988@gmail.com>
2019-01-30 21:44:45 -0500

    material working. created a new page to show in tab1

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+export interface Food {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+}
